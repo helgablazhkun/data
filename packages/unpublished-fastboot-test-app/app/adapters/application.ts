4c3708d8be5c9703041b3258200080e9f6332519
Scott Newcomer <snewcomer24@gmail.com>
2021-11-05 07:33:58 -0500

    fix fastboot test

--- packages/unpublished-fastboot-test-app/app/adapters/application.ts
+++ packages/unpublished-fastboot-test-app/app/adapters/application.ts
@@ -3,4 +3,5 @@ import RESTAdapter from '@ember-data/adapter/rest';
 export default class ApplicationAdapter extends RESTAdapter {
   defaultSerializer = 'application';
   namespace = 'api';
+  useFetch = false; // no rhyme or reason. Just illustrating the change needed for 4.0 to maintain parity. If want to use fetch, install ember-fetch
 }
