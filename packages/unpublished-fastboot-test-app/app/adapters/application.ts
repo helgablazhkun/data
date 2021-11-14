55acbfc8c86b228a0bebdd044cab08b27e67148d
Scott Newcomer <snewcomer24@gmail.com>
2021-11-14 10:43:34 -0600

    add ember-fetch

--- packages/unpublished-fastboot-test-app/app/adapters/application.ts
+++ packages/unpublished-fastboot-test-app/app/adapters/application.ts
@@ -3,5 +3,4 @@ import RESTAdapter from '@ember-data/adapter/rest';
 export default class ApplicationAdapter extends RESTAdapter {
   defaultSerializer = 'application';
   namespace = 'api';
-  useFetch = true;
 }
